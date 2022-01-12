23196b9e3d24458812fefd60b884cf9ce238d50c
User <bbelekov@mail.ru>
2022-01-12 16:51:18 +0600

    jcgfhvjbkl;m'

--- app/src/main/java/com/example/homework31/MainActivity.java
+++ app/src/main/java/com/example/homework31/MainActivity.java
@@ -2,22 +2,44 @@ package com.example.homework31;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText etMessage, etSubject, etGmail;
     private ImageView image;
 
+    private Button btnGo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        image=findViewById(R.id.image);
+        image = findViewById(R.id.image);
+        etMessage = findViewById(R.id.et_massage);
+        etSubject = findViewById(R.id.et_subject);
+        etGmail = findViewById(R.id.et_gmail);
+        btnGo=findViewById(R.id.btn_go);
         Glide.with(this).load("https://i.pinimg.com/474x/23/ab/a6/23aba60b66ef08174bb7455c4a8a2d2f.jpg").into(image);
 
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("plain/text");
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[]{etGmail.getText().toString()});
+                intent.putExtra(Intent.EXTRA_SUBJECT,etSubject.getText().toString());
+                intent.putExtra(Intent.EXTRA_TEXT,etMessage.getText().toString());
+                startActivity(intent);
+
+            }
+        });
     }
+
+
 }
